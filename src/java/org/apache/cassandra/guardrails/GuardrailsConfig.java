a6b0de7a3dd723caafef6987ee1a2cd069919faf
Daniel Jatnieks <jatnieks@pobox.com>
2021-04-30 14:18:58 -0700

    STAR-513 Add Guardrail to disable TRUNCATE TABLE
    Co-authored-by: Stefania Alborghetti <stef1927@users.noreply.github.com>

--- src/java/org/apache/cassandra/guardrails/GuardrailsConfig.java
+++ src/java/org/apache/cassandra/guardrails/GuardrailsConfig.java
@@ -96,6 +96,8 @@ public class GuardrailsConfig
 
     public Boolean read_before_write_list_operations_enabled;
 
+    public Boolean truncate_table_enabled;
+
     /**
      * Validate that the value provided for each guardrail setting is valid.
      *
@@ -145,6 +147,8 @@ public class GuardrailsConfig
      */
     public void applyConfig()
     {
+        enforceDefault(truncate_table_enabled, v -> truncate_table_enabled = v, true, true);
+
         enforceDefault(user_timestamps_enabled, v -> user_timestamps_enabled = v, true, true);
 
         enforceDefault(column_value_size_failure_threshold_in_kb, v -> column_value_size_failure_threshold_in_kb = v, NO_LIMIT, 5 * 1024L);
